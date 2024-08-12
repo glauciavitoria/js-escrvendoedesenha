d747ed2a8e189a82c8a9abbb2cf801737aa03e60
glauciavitoria <00001106261604sp@al.educacao.sp.gov.br>
2024-08-12 08:05:35 -0400

    adicionando arquvios js e css

---
+++ style.css
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+    background("purple");
+    
+  }
+  function draw() {
+  
+    stroke("red")
+    fill("pink")
+    
+    // console.log(mouseIsPressed);
+    
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+    
+  }
+  }
